cf992c3c1fd4f9f6dd6b608b2439d14dd3f7f1c2
nikita <nbarabanschikov@mts.ru>
2025-10-20 20:37:31 +0300

    add handler

--- bot.py
+++ bot.py
@@ -1,9 +1,25 @@
 from orca.orca import start
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler,ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+
+# Define a command handler. These usually take the two arguments update and context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Send a message when the command /start is issued."""
+    await update.message.reply_text("Shalom!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
 
 application = ApplicationBuilder.token("TOKEN").build()
 
 
 
+
+
 application.add_handler(CommandHandler(["start"], start))
+
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+application.run_polling(allowed_updates=Update.ALL_TYPES)
